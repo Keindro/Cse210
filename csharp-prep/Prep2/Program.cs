aa04076dd379e29b044ee9baee2c87c291fda87e
Diyaan <kpankaaristide@gmail.com>
2023-09-15 10:06:19 -0600

    fixing errors

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -13,53 +13,53 @@ class Program
 
         if(percent>= 93)
         { 
-            ter = "A";
+            letter = "A";
         }
         else if(percent >= 90)
         {
-            ter = "A-";
+            letter = "A-";
         }
         else if (percent >= 87)
         {
-            ter = "B+";
+            letter = "B+";
         }
         else if (percent >= 83)
         {
-            ter = "B";
+            letter = "B";
         }
         else if (percent >= 80)
         {
-            ter = "B-";
+            letter = "B-";
         }
         else if (percent >= 77)
         {
-            ter = "C+";
+            letter = "C+";
         }
         else if (percent >= 73)
         {
-            ter = "C";
+            letter = "C";
         }
         else if (percent >= 70)
         {
-            ter = "C-";
+            letter = "C-";
         }
         else if (percent >= 67)
         {
-            ter = "D+";
+            letter = "D+";
         }
         else if (percent >= 63)
         {
-            ter = "D";
+            letter = "D";
         }
         else if (percent >= 60)
         {
-            ter = "D-";
+            letter = "D-";
         }
         else
         {
-            ter = "F";
+            letter = "F";
         }
-        Console.WriteLine($"Your grade is {ter}.");
+        Console.WriteLine($"Your grade is {letter}.");
         if (percent >= 70 )
         {
             Console.WriteLine("Congratulations, you passed.");  
