a0f2b662f3fa0fe34ac765ca282ed7ad5a434d92
Diyaan <kpankaaristide@gmail.com>
2023-10-11 19:36:02 -0600

    Learning03 done

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,58 @@
+using System;
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _bottom = bottom;
+        _top = top;
+    }
+    
+    public int GetTopNumber()
+    {
+        return _top;
+    }
+    
+    public void SetTopNumber(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottomNumber()
+    {
+        return _bottom;
+    }
+    
+    public void SetBottomNumber(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top/(double)_bottom;
+    }
+
+}
+
+
+
+
