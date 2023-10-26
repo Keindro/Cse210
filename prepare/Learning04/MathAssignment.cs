d8fd407b0624a1128625acf1db410b81cd283688
Diyaan <kpankaaristide@gmail.com>
2023-10-26 11:48:45 -0600

    learning04

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,16 @@
+using System;
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+    public MathAssignment(string name, string topic, string textbook, string problem) : base (name, topic)
+    {
+        _textbookSection = textbook;
+        _problems = problem;
+    }
+
+    public string GetHomework()
+    {
+        return $"{GetSummary()}\n{_textbookSection} {_problems}";
+    }
+}
