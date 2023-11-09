f6233b90e6dd08472bf5b6fa8301359211f6cee4
Diyaan <kpankaaristide@gmail.com>
2023-11-08 18:52:08 -0700

    learning 05 done

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Circle : Shape
+{
+    private int _radius = 0;
+    float pi = (float)Math.PI;
+
+    public Circle(string color, int radius) : base(color)
+    {
+        _radius = radius;
+    }
+	
+    public decimal GetRadius()
+    {
+        return _radius;
+    }
+    public void SetRadius(int radius)
+    {
+        _radius = radius;
+    }
+
+    public override float GetArea()
+    {
+        return pi * _radius *_radius;
+    }
+}
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Square : Shape
+{
+    private int _side = 0;
+
+
+    public Square(string color, int side) : base(color)
+    {
+        _side = side;
+    }
+
+
+    public decimal GetSide()
+    {
+        return _side;
+    }
+
+    public void SetSide(int side)
+    {
+        _side = side;
+    }
+
+    public override float GetArea()
+    {
+        return _side * _side;
+    }
+}
